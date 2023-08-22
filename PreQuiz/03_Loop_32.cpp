9014d7d6ced2412853284b1eab01d8dae6c99ae6
agehcx <chega.malee@gmail.com>
2023-08-22 11:18:08 +0700

    Update

--- PreQuiz/03_Loop_32.cpp
+++ PreQuiz/03_Loop_32.cpp
@@ -6,8 +6,7 @@ void solve() {
     int score = 0, i = 0, lastr = -1, tmp = 0;
     bool chkconsecutive = true;
     getline(cin, inp);
-    for(auto & e : inp)
-        if(e != ' ') str += e;
+    for(auto & e : inp) if(e != ' ') str += e;
     if(str[0] != 'R') {
         cout << "WRONG_INPUT\n";
         return;
@@ -17,10 +16,7 @@ void solve() {
             cout << "WRONG_INPUT\n";
             return;
     }}
-    for(int i = 0; i < str.size(); i++) {
-        if(str[i] == 'R') lastr = i;
-        if(i+1 == str.size()) break;
-    }
+    for(int i = 0; i < str.size(); i++) if(str[i] == 'R') { lastr = i; }
     string subs = str.substr(lastr + 1 + (lastr + 2 < str.size()));
     for(int i = 0; i < str.size(); i++) score += sc.find(str[i]) + 1;
     for(int i = 0; i < str.size(); i += 2) {
@@ -30,8 +26,7 @@ void solve() {
     }}
     if(tmp != 0) subbedstr = str.substr(tmp);
     if(subbedstr.find('R') != string::npos || (subbedstr.size() > 0 && subbedstr[0] != 'Y')) chkconsecutive = false;
-    cout << ((lsq.find(subs) != string::npos && chkconsecutive)?to_string(score):"WRONG_INPUT");
-    cout << '\n';
+    cout << ((lsq.find(subs) != string::npos && chkconsecutive)?to_string(score):"WRONG_INPUT") << '\n' ;
 }
 
 
